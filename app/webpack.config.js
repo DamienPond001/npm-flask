f2bd4b73a42a7294718a369ce0ea65e2dd37886c
Damien_OLSPS <Damien@OLSPS.com>
2019-09-26 15:56:56 +0200

    update

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,21 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        index: ["./src/js/index.js"],
-        page1: ["./src/js/page1.js"],
-        page2: ["./src/js/page2.js"]
-    },
-    output: {
-        path: path.resolve(__dirname, './static/dist'),
-        filename: "js/[name]-bundle.js"
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-          }
-        ]
-      }    
+	entry: {
+		index: ['./src/js/index.js'],
+		page1: ['./src/js/page1.js'],
+		page2: ['./src/js/page2.js']
+	},
+	output: {
+		path: path.resolve(__dirname, './static'),
+		filename: 'js/[name]-bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	}
 };
